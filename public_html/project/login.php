017b48f3efb13d31ce607a3964325e81ef72d5f2
Michos Colobong <michosrcolobong@gmail.com>
2026-07-17 22:38:09 -0400

    Add user roles authorization

--- public_html/project/login.php
+++ public_html/project/login.php
@@ -51,6 +51,7 @@ if (isset($_POST["email"], $_POST["password"])) {
         $user["user_id"] = (int)$user["user_id"];
         unset($user["password_hash"]);
 
+        $user["roles"] = get_user_roles($user["user_id"]);
         $_SESSION["user"] = $user;
 
         flash("Welcome back.", "success");
